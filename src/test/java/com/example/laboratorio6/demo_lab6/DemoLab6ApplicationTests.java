e7bad7f0b2c4d85b67211ad876fbbde0d538658c
Wicho0307 <luispedroxd07@gmail.com>
2026-09-18 12:35:15 -0600

    Primer commit para la estructura del laboratorio

---
+++ src/test/java/com/example/laboratorio6/demo_lab6/DemoLab6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio6.demo_lab6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLab6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
